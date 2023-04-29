9c5751c3cca8ed61d188a4022f5e53511df439f6
Ahmad Sadaqat <ahmadsadaqat6161@gmail.com>
2023-04-30 02:41:07 +0500

    added hosts routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./pages/App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
 
-function About() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>This is a test of the Vite + React integration.</p>
-    </>
-  );
-}
+import Vans from "./pages/Vans/Vans.jsx";
+import About from "./pages/About.jsx";
+import VanDetail from "./pages/Vans/VanDetail.jsx";
+import Layout from "./components/Layout.jsx";
+import Dashboard from "./pages/Host/Dashboard.jsx";
+import Income from "./pages/Host/Income.jsx";
+import Reviews from "./pages/Host/Reviews.jsx";
+import HostLayout from "./components/HostLayout.jsx";
+import HostVans from "./pages/Host/HostVans.jsx";
+import HostVanDetail from "./pages/Host/HostVanDetail.jsx";
+import HostVanInfo from "./pages/Host/HostVanInfo.jsx";
+import HostVanPricing from "./pages/Host/HostVanPricing.jsx";
+import HostVanPhotos from "./pages/Host/HostVanPhotos.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <header>
-        <Link to="/">#VANLIFE</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
